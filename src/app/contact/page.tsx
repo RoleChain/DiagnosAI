32f2a67a537928c11000b58bb4aa3e23b987c3e4
Harsh Raj <32563286+Harsh48@users.noreply.github.com>
2025-09-28 22:09:42 +0530

    fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -40,7 +40,7 @@ const contactMethods = [
 		icon: MapPin,
 		title: 'Office Address',
 		description: 'Visit us at our headquarters',
-		contact: '#100, 104, S R arcade, 6th Cross, Tulsi Theatre Road, Marathahalli, Bangalore -560037',
+		contact: '#104, S R Arcade, 6th Cross, Tulsi Theatre Road, Marathahalli, Bangalore -560037',
 		action: '#'
 	}
 ];
